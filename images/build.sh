e10138dc11e75f88901c6674b80e4917731121dd
Zain Malik <zmalikshxil@gmail.com>
2019-12-16 15:31:54 +0100

    add image for cruise-control
    This PR adds a docker image for cruise-control.
    To enable multiple docker images in the repository. The images/build.sh has been refactored
    to support multiple docker images. As for now for kafka and cruise-control
    
    `build.sh --image=kafka` to build kafka image
    `build.sh --image=cruise-control` to build cruise-control image
    `build.sh --image=kafka --push` to build and push an image
    
    Signed-off-by: Zain Malik <zmalikshxil@gmail.com>

--- images/build.sh
+++ images/build.sh
@@ -2,9 +2,60 @@
 
 set -eu
 
-source docker-version.sh
-docker image build --build-arg KAFKA_VERSION=${KAFKA_BASE_TECH_VERSION} -t mesosphere/kafka:${DOCKER_IMAGE_VERSION}-${KAFKA_BASE_TECH_VERSION} .
+function print_help() {
+  echo "Usage: ${0} [OPTIONS]"
+  echo
+  echo "OPTIONS:"
+  echo "--push             push the recently build image"
+  echo "--image            docker image to build. valid options are '-image=kafka' and '-image=cruise-control'"
+  echo
+}
 
-if [[ $1 == "push" ]]; then
-    docker push mesosphere/kafka:${DOCKER_IMAGE_VERSION}-${KAFKA_BASE_TECH_VERSION}
+PUSH_IMAGE="false"
+for arg in "$@"
+do
+    case $arg in
+        -p|--push)
+        PUSH_IMAGE="true"
+        shift
+        ;;
+        -i=*|--image=*)
+        IMAGE_NAME="${arg#*=}"
+        shift
+        ;;
+        *)
+        print_help
+        exit 1
+        ;;
+    esac
+done
+
+if [[ -z "${IMAGE_NAME:-}" ]]; then
+  print_help
 fi
+
+if [[ "${IMAGE_NAME}" == "kafka" ]]; then
+  source kafka/docker-version.sh
+  docker image build --build-arg KAFKA_VERSION=${KAFKA_BASE_TECH_VERSION} -t mesosphere/kafka:${KAFKA_DOCKER_IMAGE_VERSION}-${KAFKA_BASE_TECH_VERSION} ./kafka
+elif [[ "${IMAGE_NAME}" == "cruise-control" ]]; then
+  source cruise-control/docker-version.sh
+  docker image build --build-arg CRUISE_CONTROL_VERSION=${CRUISE_CONTROL_VERSION} --build-arg CRUISE_CONTROL_UI_VERSION=${CRUISE_CONTROL_UI_VERSION} \
+    -t mesosphere/cruise-control:${CRUISE_CONTROL_DOCKER_IMAGE_VERSION} ./cruise-control
+else
+  print_help
+fi
+
+
+if [[ "${PUSH_IMAGE}" == "true" ]]; then
+  if [[ ${IMAGE_NAME} == "cruise-control" ]]; then
+    docker push mesosphere/cruise-control:${CRUISE_CONTROL_DOCKER_IMAGE_VERSION}
+  fi
+  if [[ "${IMAGE_NAME}" == "kafka" ]]; then
+    docker push mesosphere/kafka:${KAFKA_DOCKER_IMAGE_VERSION}-${KAFKA_BASE_TECH_VERSION}
+  fi
+else
+  echo "Image built successfully."
+  echo "To push the image use the '--push' flag"
+fi
+
+exit 0
